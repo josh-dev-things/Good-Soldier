7441d071e14cfcb78879775ac105c71ddf8f39b3
Josh <ethankessler.dev@gmail.com>
2023-09-28 19:34:05 +0100

    Impl Executor.java

--- Interpreter/Interfaces/IExecutor.java
+++ Interpreter/Interfaces/IExecutor.java
@@ -12,28 +12,4 @@ public interface IExecutor {
      * @return integer. < 0 indicates error.
      */
     public int execute(ITokenMap tM);
-    
-    abstract ITokenMap executeComparison(ITokenMap tM);
-
-    abstract ITokenMap executeBOperator(ITokenMap tM);
-
-    abstract ITokenMap executeOperator(ITokenMap tM);
-
-    abstract ITokenMap executeKeyword(ITokenMap tM);
-
-    abstract ITokenMap executeAssignment(ITokenMap tM);
-
-    /**
-     * This is me being funny. Good Soldier Script etc. so internally "reporting" is the same as "speaking" or outputting something. Let me have fun.
-     * @param string will be output to console irrespective of debug argument
-     */ 
-    abstract void report(String string);
-
-    /**
-     * Pops element from array and returns new array of length n-1
-     * @param arr
-     * @param index
-     * @return New String[] with specified element removed.
-     */
-    abstract String[] popIndexInStringArray(String[] arr, int index);
 }
